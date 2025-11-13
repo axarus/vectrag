01a839155204de6ac26709639361c043fe048f60
axarus <allanraza@hotmail.com>
2025-11-13 00:56:48 -0500

    feat: add validation status

--- internal/domain/status.go
+++ internal/domain/status.go
@@ -1,8 +1,17 @@
 package domain
 
+import "fmt"
+
 type Status string
 
 const (
 	StatusDraft   Status = "draft"
 	StatusPublish Status = "publish"
 )
+
+func ValidateStatus(status Status) error {
+	if status != StatusDraft && status != StatusPublish {
+		return fmt.Errorf("must be either 'draft' or 'publish'")
+	}
+	return nil
+}
