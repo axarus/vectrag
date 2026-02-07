8607658502d15241f3f7fc31d4c75cdbefe19a8a
axarus <allanraza@hotmail.com>
2026-02-07 00:18:59 -0500

    feat: adds status delete on domain

--- internal/domain/status.go
+++ internal/domain/status.go
@@ -7,11 +7,12 @@ type Status string
 const (
 	StatusDraft   Status = "draft"
 	StatusPublish Status = "publish"
+	StatusDelete  Status = "delete"
 )
 
 func ValidateStatus(status Status) error {
-	if status != StatusDraft && status != StatusPublish {
-		return fmt.Errorf("must be either 'draft' or 'publish'")
+	if status != StatusDraft && status != StatusPublish && status != StatusDelete {
+		return fmt.Errorf("must be either 'draft', 'publish', or 'delete'")
 	}
 	return nil
 }
